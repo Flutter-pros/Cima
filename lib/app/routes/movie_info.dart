c4a0b76802c4592865dc86ee104eff8a6554023f
Ashraf Khairy <102880187+ashrafkhairy@users.noreply.github.com>
2022-08-27 13:55:09 +0200

    Update movie_info.dart

--- lib/app/routes/movie_info.dart
+++ lib/app/routes/movie_info.dart
@@ -1,18 +1,21 @@
 import 'package:flutter/material.dart';
+import 'package:n_project/ml/utils/iconbutton.dart';
+import 'package:n_project/ml/utils/rowanddetails.dart';
 
-class MovieInfoScreen extends StatefulWidget {
-  const MovieInfoScreen({Key? key}) : super(key: key);
+import 'package:n_project/ml/utils/text.dart';
+
+class MovienfoScreen extends StatefulWidget {
+  MovienfoScreen({Key? key}) : super(key: key);
 
   @override
-  State<MovieInfoScreen> createState() => _MovieInfoScreenState();
+  State<MovienfoScreen> createState() => _MovienfoScreenState();
 }
 
-class _MovieInfoScreenState extends State<MovieInfoScreen> {
+class _MovienfoScreenState extends State<MovienfoScreen> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      backgroundColor: const Color(0xff0c0f21),
-      body: SizedBox(
+      body: Container(
         height: double.infinity,
         width: double.infinity,
         child: ListView(
@@ -20,15 +23,7 @@ class _MovieInfoScreenState extends State<MovieInfoScreen> {
             Stack(
               alignment: Alignment.center,
               children: [
-                SizedBox(
-                  // decoration: BoxDecoration(boxShadow: [
-                  //   BoxShadow(
-                  //       color: Colors.black,
-                  //       spreadRadius: 1,
-                  //       offset: Offset(-10, -5),
-                  //       blurRadius: 10,
-                  //       blurStyle: BlurStyle.normal)
-                  // ]),
+                Container(
                   height: 250,
                   width: MediaQuery.of(context).size.width / 1,
                   child: Image.network(
@@ -39,19 +34,22 @@ class _MovieInfoScreenState extends State<MovieInfoScreen> {
                 Positioned(
                     right: 2,
                     top: 5,
-                    child: IconButton(
-                        onPressed: () {},
+                    child: IconButtonUtils(
                         icon: const Icon(
                           Icons.arrow_back,
-                          color: Colors.black,
-                        ))),
+                          color: Colors.white,
+                        ),
+                        onprassed: () {})),
                 Positioned(
                   child: CircleAvatar(
-                    maxRadius: 30,
-                    backgroundColor: Colors.black,
-                    child: IconButton(
-                        onPressed: () {}, icon: const Icon(Icons.play_arrow)),
-                  ),
+                      maxRadius: 30,
+                      backgroundColor: Colors.black,
+                      child: IconButtonUtils(
+                          icon: const Icon(
+                            Icons.play_arrow,
+                            color: Colors.white,
+                          ),
+                          onprassed: () {})),
                 ),
                 Positioned(
                   bottom: 0,
@@ -63,11 +61,12 @@ class _MovieInfoScreenState extends State<MovieInfoScreen> {
                     ),
                     height: 40,
                     width: 80,
-                    child: const Center(
-                      child: Text(
-                        style: TextStyle(color: Colors.black),
-                        " 1h 20m  ",
-                        textAlign: TextAlign.end,
+                    child: Center(
+                      child: TextUtils(
+                        color: Colors.white,
+                        fontWeight: FontWeight.normal,
+                        text: " 1h 20m  ",
+                        textalign: TextAlign.end,
                       ),
                     ),
                   ),
@@ -90,74 +89,13 @@ class _MovieInfoScreenState extends State<MovieInfoScreen> {
             ),
             SizedBox(
               width: double.infinity,
-              child: Row(
-                mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                children: [
-                  Container(
-                      width: 50,
-                      decoration: BoxDecoration(
-                        borderRadius: BorderRadius.circular(5),
-                        color: Colors.grey.withOpacity(.1),
-                      ),
-                      height: 40,
-                      child: Center(
-                        child: IconButton(
-                            color: Colors.white,
-                            onPressed: () {},
-                            icon: const Icon(Icons.share)),
-                      )),
-                  Container(
-                    width: 50,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(7),
-                      color: Colors.grey.withOpacity(.1),
-                    ),
-                    height: 40,
-                    child: Row(
-                        mainAxisAlignment: MainAxisAlignment.spaceEvenly,
-                        children: const [
-                          Text(
-                            "5.0",
-                            textAlign: TextAlign.end,
-                            style: TextStyle(color: Colors.white),
-                          ),
-                          Icon(
-                            Icons.star,
-                            color: Colors.yellow,
-                            size: 20,
-                          )
-                        ]),
+              child: Column(
+                children: const [
+                  DetailsRow(),
+                  SizedBox(
+                    height: 5,
                   ),
-                  Container(
-                    width: 70,
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(7),
-                      color: Colors.grey.withOpacity(.1),
-                    ),
-                    height: 40,
-                    child: const Center(
-                      child: Text(
-                        "جريمه",
-                        textAlign: TextAlign.end,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
-                  Container(
-                    decoration: BoxDecoration(
-                      borderRadius: BorderRadius.circular(7),
-                      color: Colors.grey.withOpacity(.1),
-                    ),
-                    height: 40,
-                    width: 35,
-                    child: const Center(
-                      child: Text(
-                        "+18",
-                        textAlign: TextAlign.end,
-                        style: TextStyle(color: Colors.white),
-                      ),
-                    ),
-                  ),
+                  DetailsRow(),
                 ],
               ),
             ),
@@ -168,23 +106,26 @@ class _MovieInfoScreenState extends State<MovieInfoScreen> {
               margin: const EdgeInsets.only(right: 20, left: 20),
               child: Column(
                 mainAxisAlignment: MainAxisAlignment.start,
-                crossAxisAlignment: CrossAxisAlignment.start,
-                children: const [
-                  Text(
-                    "وصف الفيلم  :",
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
+                crossAxisAlignment: CrossAxisAlignment.end,
+                children: [
+                  TextUtils(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    text: "وصف الفيلم  ",
+                    textalign: TextAlign.end,
                   ),
-                  Text(
-                    "يتم اختيار فيوليت رودريغيز ، وهي مراهقة أمريكية مكسيكية يومية ، بواسطة قناع لوشادور السحري الذي يحولها إلى الترا فولت ، وهي بطل خارق يقاتل الجريمة إلى جانب عمها المتعصب ، كروز ، المعروف أيضًا باسم العقرب الأسود.",
-                    style: TextStyle(
-                      color: Colors.white,
-                    ),
+                  TextUtils(
+                    fontWeight: FontWeight.normal,
+                    color: Colors.white,
+                    text:
+                        "يتم اختيار فيوليت رودريغيز ، وهي مراهقة أمريكية مكسيكية يومية ، بواسطة قناع لوشادور السحري الذي يحولها إلى الترا فولت ، وهي بطل خارق يقاتل الجريمة إلى جانب عمها المتعصب ، كروز ، المعروف أيضًا باسم العقرب الأسود.",
+                    textalign: TextAlign.end,
                   ),
-                  Text(
-                    "ذات صله     :",
-                    style: TextStyle(
-                        color: Colors.white, fontWeight: FontWeight.bold),
+                  TextUtils(
+                    fontWeight: FontWeight.bold,
+                    color: Colors.white,
+                    text: "ذات صله ",
+                    textalign: TextAlign.end,
                   ),
                 ],
               ),
@@ -196,15 +137,6 @@ class _MovieInfoScreenState extends State<MovieInfoScreen> {
                 child: Stack(
                   alignment: Alignment.topRight,
                   children: [
-                    // Blur(
-                    //   blur: 200,
-                    //   blurColor: const Color(0xff22202f).withOpacity(.1),
-                    //   child: Container(
-                    //       decoration: const BoxDecoration(
-                    //           image: DecorationImage(
-                    //               image: AssetImage("images/10.PNG"),
-                    //               fit: BoxFit.cover))),
-                    // ),
                     ListView.builder(
                         itemCount: 17,
                         scrollDirection: Axis.horizontal,
@@ -219,7 +151,7 @@ class _MovieInfoScreenState extends State<MovieInfoScreen> {
                             child: Column(
                               mainAxisAlignment: MainAxisAlignment.start,
                               children: [
-                                SizedBox(
+                                Container(
                                   height: 170,
                                   width: 100,
                                   child: ClipRRect(
@@ -239,11 +171,12 @@ class _MovieInfoScreenState extends State<MovieInfoScreen> {
                                 Column(
                                   mainAxisAlignment: MainAxisAlignment.end,
                                   crossAxisAlignment: CrossAxisAlignment.end,
-                                  children: const [
-                                    Text(
-                                      "title",
-                                      style: TextStyle(color: Colors.white),
-                                    ),
+                                  children: [
+                                    TextUtils(
+                                        text: "title",
+                                        textalign: TextAlign.end,
+                                        color: Colors.white,
+                                        fontWeight: FontWeight.bold)
                                   ],
                                 )
                               ],
@@ -263,60 +196,3 @@ class _MovieInfoScreenState extends State<MovieInfoScreen> {
     );
   }
 }
-
-
-
-
-
-
-
-//  SizedBox(
-//               height: 250,
-//               width: MediaQuery.of(context).size.width / 1,
-//               child: Stack(children: [
-//                 ListView.builder(
-//                   scrollDirection: Axis.horizontal,
-//                   itemBuilder: ((context, index) {
-//                     return Card(
-//                       shape: const RoundedRectangleBorder(
-//                           borderRadius: BorderRadius.only(
-//                         bottomLeft: Radius.circular(25),
-//                         topRight: Radius.circular(30),
-//                       )),
-//                       color: Color(0xff23273b),
-//                       child: Column(
-//                         mainAxisAlignment: MainAxisAlignment.center,
-//                         children: [
-//                           SizedBox(
-//                             height: 190,
-//                             width: MediaQuery.of(context).size.width / 3,
-//                             child: ClipRRect(
-//                               borderRadius: BorderRadius.circular(10),
-//                               child: Image.network(
-//                                 fit: BoxFit.fill,
-//                                 "https://mycima.fun/wp-content/uploads/2022/06/%D9%85%D8%B3%D9%84%D8%B3%D9%84-In-the-Dark-%D8%A7%D9%84%D9%85%D9%88%D8%B3%D9%85-%D8%A7%D9%84%D8%B1%D8%A7%D8%A8%D8%B9-347x520.jpg",
-//                                 errorBuilder: (BuildContext context,
-//                                     Object exception, StackTrace? stackTrace) {
-//                                   return Image.asset(
-//                                       "images/failLoading/image_fail_loading.jpeg");
-//                                 },
-//                               ),
-//                             ),
-//                           ),
-//                           Column(
-//                             mainAxisAlignment: MainAxisAlignment.end,
-//                             crossAxisAlignment: CrossAxisAlignment.end,
-//                             children: [
-//                               Text(
-//                                 "a",
-//                               ),
-//                             ],
-//                           )
-//                         ],
-//                       ),
-//                     );
-//                   }),
-//                   itemCount: 15,
-//                 ),
-//               ]),
-//             ),
