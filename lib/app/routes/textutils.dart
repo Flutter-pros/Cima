7b00b1d380e59e28c679472f5e385148ca69538c
Ashraf Khairy <102880187+ashrafkhairy@users.noreply.github.com>
2022-08-27 13:58:47 +0200

    Create textutils.dart

---
+++ lib/app/routes/textutils.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+
+class TextUtils extends StatelessWidget {
+  TextUtils(
+      {Key? key,
+      required this.text,
+      required this.textalign,
+      required this.color,
+      required this.fontWeight})
+      : super(key: key);
+  final String text;
+  final TextAlign textalign;
+  final Color color;
+  final FontWeight fontWeight;
+
+  @override
+  Widget build(BuildContext context) {
+    return Text(
+      text,
+      textAlign: textalign,
+      style: TextStyle(color: color, fontWeight: FontWeight.bold),
+    );
+  }
+}
